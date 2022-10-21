4f749417443e0a01c0f79f4d72a1cea6c0115a19
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2022-10-21 12:24:04 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,10 +1,7 @@
 from application import app, greet
 
-def test_quick():
-  a = "jeff"
-  greeting = greet(a)
-  assert greeting == "Hi jeff "
 
-# def test_home_page():
-#     response = app.test_client().get('/')
-#     assert response.status_code == 200
+
+ def test_home_page():
+    response = app.test_client().get('/')
+    assert response.status_code == 200
