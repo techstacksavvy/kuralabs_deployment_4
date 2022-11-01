d2271a7aed4e17a9233e69270dfa15a880d3257b
Sasheeny Hubbard <84424434+techstacksavvy@users.noreply.github.com>
2022-10-31 23:23:01 -0500

    Update deploy.sh

--- intTerraform/deploy.sh
+++ intTerraform/deploy.sh
@@ -5,9 +5,9 @@ sudo apt -y install git
 sleep 3
 sudo apt -y install python3-pip
 sleep 3
-git clone https://github.com/kura-labs-org/kuralabs_deployment_3.git
+git clone https://github.com/kura-labs-org/kuralabs_deployment_4.git
 sleep 2
-cd kuralabs_deployment_3/
+cd kuralabs_deployment_4/
 pip install -r requirements.txt
 pip install gunicorn
 python3 -m gunicorn -w 4 application:app -b 0.0.0.0 --daemon
