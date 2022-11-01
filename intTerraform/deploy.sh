91ecf5d00ef06073cab4508400caed7a744470a5
Sasheeny Hubbard <84424434+techstacksavvy@users.noreply.github.com>
2022-10-31 22:31:41 -0500

    update deploy.sh
    changed line 8 back to deployment 3

--- intTerraform/deploy.sh
+++ intTerraform/deploy.sh
@@ -5,7 +5,7 @@ sudo apt -y install git
 sleep 3
 sudo apt -y install python3-pip
 sleep 3
-git clone https://github.com/kura-labs-org/kuralabs_deployment_4.git
+git clone https://github.com/kura-labs-org/kuralabs_deployment_3.git
 sleep 2
 cd kuralabs_deployment_3/
 pip install -r requirements.txt
